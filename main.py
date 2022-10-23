80265c3329f0992bb015f5b00dbf4770db786478
Justin Riddiough <jriddiough@gmail.com>
2022-10-23 02:29:40 -0500

    Adding updates

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from PySide6.QtWidgets import QApplication
-import sys
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
+import sys, os
 
 from widgets.main_window import MainWindow
+basedir_icons = os.path.dirname(__file__) + "\icons\\"
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
@@ -9,6 +11,26 @@ if __name__ == "__main__":
   window = MainWindow()
   window.show()
   window.resize(1000, 600)
-  # app.setQuitOnLastWindowClosed(False)
+
+  ## Tray Icon
+  app.setQuitOnLastWindowClosed(False)
+  # Create the icon
+  icon = QIcon(os.path.join(basedir_icons, 'icon.png'))
+
+  # Create the tray
+  tray = QSystemTrayIcon()
+  tray.setIcon(icon)
+  tray.setVisible(True)
+
+  # Create the menu
+  menu = QMenu()
+
+  # Add a Quit option to the menu.
+  quit = QAction("Quit")
+  quit.triggered.connect(app.quit)
+  menu.addAction(quit)
+
+  # Add the menu to the tray
+  tray.setContextMenu(menu)
 
 sys.exit(app.exec())
