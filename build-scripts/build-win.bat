0f70944b365ffa8f9eeaf44542b164665553f8e0
Justin Riddiough <jriddiough@gmail.com>
2022-10-23 04:19:17 -0500

    Build scripts & pipenv build

--- build-scripts/build-win.bat
+++ build-scripts/build-win.bat
@@ -1,5 +1,7 @@
-if exist ldm\Scripts\activate (
-  ldm\venv\Scripts\activate & pip3 install -r requirements.txt & pyinstaller.exe main.spec -y
-) else (
-  python.exe -m venv ldm & ldm\Scripts\activate & pip3 install -r requirements.txt & pyinstaller.exe main.spec -y
-)
+@REM if exist ldm\Scripts\activate (
+@REM   ldm\venv\Scripts\activate & pip3 install -r requirements.txt & pyinstaller.exe main.spec -y
+@REM ) else (
+@REM   python.exe -m venv ldm & ldm\Scripts\activate & pip3 install -r requirements.txt & pyinstaller.exe main.spec -y
+@REM )
+
+pipenv run pyinstaller main.spec -y
